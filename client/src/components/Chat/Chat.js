be317ec6ab5c9867f09d18ad9253317490b5a0f2
Akhamis01 <aserkhamis@gmail.com>
2021-07-21 13:30:52 +0400

    first commit

---
+++ client/src/components/Chat/Chat.js
@@ -0,0 +1,74 @@
+import React, {useState, useEffect} from 'react';
+import querystring from 'query-string';
+import io from 'socket.io-client';
+
+import TextContainer from '../TextContainer/TextContainer';
+import InfoBar from '../InfoBar/InfoBar';
+import Input from '../Input/Input';
+import Messages from '../Messages/Messages';
+import './Chat.css';
+
+let socket;
+const ENDPOINT = 'https://react-chat-project1.herokuapp.com/';
+
+const Chat = ({ location }) => {
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+    const [users, setUsers] = useState('');
+    const [message, setMessage] = useState('');
+    const [messages, setMessages] = useState([]);
+
+    useEffect( () => {
+        const data = querystring.parse(location.search);
+        const {name, room} = data;
+
+        socket = io(ENDPOINT);
+        setName(name);
+        setRoom(room);
+
+        socket.emit('join', { name, room }, (error) => {
+            if(error){
+                alert(error);
+            }
+        });
+
+    }, [location.search]);
+
+
+
+    useEffect( () => {
+
+        socket.on('message', (message) => {
+            setMessages(messages => [...messages, message]);
+        });
+
+        socket.on('roomData', ({ users }) => {
+            setUsers(users);
+        });
+
+    }, []);
+
+
+
+    const sendMessage = (event) => {
+        event.preventDefault();
+
+        if(message){
+            socket.emit('sendMessage', message, () => setMessage(''));
+        }
+    }
+ 
+
+    return (
+        <div className="outerContainer">
+            <div className="container">
+                <InfoBar room={room}/>
+                <Messages messages={messages} name={name}/>
+                <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
+            </div>
+            <TextContainer users={users} />
+        </div>
+    );
+};
+
+export default Chat;
